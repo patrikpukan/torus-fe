642fa22b6300c7dd8702bd3f5cb30a6c2e43b636
Dávid Vikor <101962943+vikd00@users.noreply.github.com>
2025-10-31 01:06:30 +0100

    chore: disable eslint rule for explicit any in apolloClient

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { SetContextLink } from "@apollo/client/link/context";

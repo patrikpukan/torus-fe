7fefdcd5b55b978f1b328c2642073db9e9a357ad
Erik Le <erik.le@outlook.com>
2025-11-11 09:35:40 +0100

    feat(calendar): add pause and resume activity mutations and query

---
+++ src/features/calendar/graphql/pause-activity.mutations.ts
@@ -0,0 +1,55 @@
+import { useMutation, useQuery } from '@apollo/client/react';
+import { graphql } from 'gql.tada';
+
+export const PAUSE_ACTIVITY_MUTATION = graphql(`
+  mutation PauseActivity($input: PauseActivityInput!) {
+    pauseActivity(input: $input) {
+      id
+      userId
+      type
+      title
+      description
+      startDateTime
+      endDateTime
+      createdAt
+    }
+  }
+`);
+
+export const RESUME_ACTIVITY_MUTATION = graphql(`
+  mutation ResumeActivity {
+    resumeActivity
+  }
+`);
+
+export const GET_ACTIVE_PAUSE_QUERY = graphql(`
+  query GetActivePause($startDate: DateTime!, $endDate: DateTime!) {
+    expandedCalendarOccurrences(
+      startDate: $startDate
+      endDate: $endDate
+    ) {
+      id
+      occurrenceStart
+      occurrenceEnd
+      originalEvent {
+        id
+        type
+        title
+        description
+        startDateTime
+        endDateTime
+        deletedAt
+      }
+    }
+  }
+`);
+
+// Custom hooks
+export const usePauseActivityMutation = () => 
+  useMutation(PAUSE_ACTIVITY_MUTATION);
+
+export const useResumeActivityMutation = () => 
+  useMutation(RESUME_ACTIVITY_MUTATION);
+
+export const useActivePauseQuery = (variables: { startDate: string; endDate: string }) =>
+  useQuery(GET_ACTIVE_PAUSE_QUERY, { variables });
